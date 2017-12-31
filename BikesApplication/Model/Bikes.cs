9a9e620d6466a45454b558369e29321c56f99480
Abhishek Badola <abhishek.badola@nowfloats.com>
2017-12-31 09:19:43 +0530

    final push for Bikes App

---
+++ BikesApplication/Model/Bikes.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BikesApplication.Model
+{
+    public class Garage // Garage in a family
+    {
+        // List of property Bike names for each family's garage
+        public List<string> bikes { get; set; }
+    }
+}
---
+++ BikesApplication/Program.cs
@@ -0,0 +1,51 @@
+﻿using BikesApplication.Model;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace BikesApplication
+{
+    class Program
+    {
+        // Declaring file path for input json file and output json file
+        private const string filePath = @"c:\users\admin\source\repos\BikesApplication\BikesApplication\App_Data\{0}";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader readfile = File.OpenText(String.Format(filePath, "bikes.json")))
+                {
+                    // Reading from the JSON file
+                    JsonSerializer serializer = new JsonSerializer();
+
+                    // Deserializing the data from JSON file
+                    Garage[] garage = (Garage[])serializer.Deserialize(readfile, typeof(Garage[]));
+
+                    // Applying LINQ query to filter bike names and there counts (number of responses)
+                    var result = garage.SelectMany(g => g.bikes)
+                        .GroupBy(s => s) // Grouping by bike names
+                        .Select(b => new { bikeName = b.Key, totalResponses = b.Count() }) // Mapping bike name and counts relevant to the bike response
+                        .OrderByDescending(r => r.totalResponses) // Arranging in descending order i.e. The top responses first
+                        .Take(20); // Take only top 20 results
+
+                    // Writing Output to a file "output.json"
+                    File.WriteAllText(String.Format(filePath, "output.json"), JsonConvert.SerializeObject(result));
+
+                    // Showing output on the console
+                    foreach (var item in result)
+                    {
+                        Console.WriteLine("Name: {0}, Count: {1}", item.bikeName, item.totalResponses);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey(); // Holding console screen till a key is pressed
+        }
+    }
+}
